7db2a7f4b2f684d526f8c35417cdf89931522d8b
mrgrtrt <ivanovaritavitalevna@gmail.com>
2016-11-23 20:59:30 +0300

    added Summary and Plate objects / Summary Section / Plate Test

--- src/main/java/epam/course/pageobjects/SummarySection.java
+++ src/main/java/epam/course/pageobjects/SummarySection.java
@@ -3,18 +3,25 @@ package epam.course.pageobjects;
 import com.epam.jdi.uitests.core.interfaces.common.IButton;
 import com.epam.jdi.uitests.web.selenium.elements.complex.RadioButtons;
 import com.epam.jdi.uitests.web.selenium.elements.composite.Section;
+import epam.course.entities.Summary;
 import epam.course.enums.EvenRadio;
 import epam.course.enums.OddsRadio;
 import org.openqa.selenium.support.FindBy;
 
 /**
- * Created by Rita on 21.11.2016.
+ * Created by Rita on 23.11.2016.
  */
-public class Summary extends Section {
+public class SummarySection extends Section {
     @FindBy(css = "#odds-selector .radio")
     public RadioButtons<OddsRadio> odds;
     @FindBy(css = "#even-selector .radio")
     public RadioButtons<EvenRadio> evens;
     @FindBy(id = "calculate-button")
     public IButton calculate;
+
+    public void submitSummary(Summary summary) {
+        odds.select(summary.oddsRadio);
+        evens.select(summary.evenRadio);
+        calculate.click();
+    }
 }
